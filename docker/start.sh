00331979b729b4a77b1b2f0579ea6b4f57e6b5a6
Ales Teska <ales.teska@teskalabs.com>
2018-09-23 10:11:59 +0200

    Improve a docker.

--- docker/start.sh
+++ docker/start.sh
@@ -1,13 +1,7 @@
 #!/bin/ash
 
-echo ""
-echo ""
-echo ""
 echo "Welcome to a CoinGame!"
 echo "More info at https://github.com/TeskaLabs/coingame"
-echo ""
-echo ""
-echo ""
 
 RABBITMQ_NODENAME="coingamesvrmq"
 export RABBITMQ_NODENAME
@@ -19,10 +13,13 @@ export RABBITMQ_PWD
 sleep 5
 
 /opt/rabbitmq_server/sbin/rabbitmqctl delete_user guest
+
 /opt/rabbitmq_server/sbin/rabbitmqctl add_user coingamesvr ${RABBITMQ_PWD}
 /opt/rabbitmq_server/sbin/rabbitmqctl set_permissions -p / coingamesvr "" "amq.topic" ""
 
-echo ""
+/opt/rabbitmq_server/sbin/rabbitmqctl add_user coingameminer guest
+/opt/rabbitmq_server/sbin/rabbitmqctl set_permissions -p / coingameminer "~T.*" "~T.*" "amq.topic|~T.*"
+
 echo "Starting ..."
 
 (cd /opt/coingame && /usr/bin/python3.6 coingamesvr.py -c /opt/coingame/etc/coingamesvr.conf)
